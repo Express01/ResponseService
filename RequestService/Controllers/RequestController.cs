bd015d9ca3335baefeba7668ae902f671f18b8f6
Express01 <115688744+Express01@users.noreply.github.com>
2023-05-21 19:56:57 +0300

    Добавить службу запросов

---
+++ RequestService/Controllers/RequestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RequestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestController : ControllerBase
+    {
+        //Get api/request
+        [HttpGet]
+        public async Task<ActionResult> MakeRequest()
+        {
+          var client=new HttpClient();
+            var response = await client.GetAsync("https://localhost:7213/api/Response/25");
+            if (response.IsSuccessStatusCode)
+            {
+                 Console.WriteLine("--> ResponseService returned Succes");
+                return Ok();
+            }
+            Console.WriteLine("--> ResponseService returned Failure");
+            return StatusCode(StatusCodes.Status500InternalServerError);
+
+        }
+    }
+}
